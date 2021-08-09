63fa7876fdf19b1a6f0cb5d40711fd9ca05c9f9a
Fabian Salas <sfabian.orozco@gmail.com>
2021-08-08 23:52:24 -0400

    first commit

--- app/controllers/pet_histories_controller.rb
+++ app/controllers/pet_histories_controller.rb
@@ -5,6 +5,8 @@ class PetHistoriesController < ApplicationController
   # GET /pet_histories.json
   def index
     @pet_histories = PetHistory.all
+    # pet/:pet_id/pet_histories
+    @pet = Pet.find(params[:pet_id])
   end
 
   # GET /pet_histories/1
@@ -15,20 +17,23 @@ class PetHistoriesController < ApplicationController
   # GET /pet_histories/new
   def new
     @pet_history = PetHistory.new
+    @pet = Pet.find(params[:pet_id])
   end
 
   # GET /pet_histories/1/edit
   def edit
+    @pet = Pet.find(params[:pet_id])
   end
 
   # POST /pet_histories
   # POST /pet_histories.json
   def create
     @pet_history = PetHistory.new(pet_history_params)
-
+    @pet = Pet.find(params[:pet_id])
+    @pet_history.pet= @pet
     respond_to do |format|
       if @pet_history.save
-        format.html { redirect_to @pet_history, notice: 'Pet history was successfully created.' }
+        format.html { redirect_to  pet_pet_history_path(@pet.id, @pet_history.id), notice: 'Pet history was successfully created.' }
         format.json { render :show, status: :created, location: @pet_history }
       else
         format.html { render :new }
@@ -40,9 +45,10 @@ class PetHistoriesController < ApplicationController
   # PATCH/PUT /pet_histories/1
   # PATCH/PUT /pet_histories/1.json
   def update
+    @pet = Pet.find(params[:pet_id])
     respond_to do |format|
       if @pet_history.update(pet_history_params)
-        format.html { redirect_to @pet_history, notice: 'Pet history was successfully updated.' }
+        format.html { redirect_to pet_pet_history_path(@pet.id, @pet_history.id), notice: 'Pet history was successfully updated.' }
         format.json { render :show, status: :ok, location: @pet_history }
       else
         format.html { render :edit }
@@ -56,7 +62,7 @@ class PetHistoriesController < ApplicationController
   def destroy
     @pet_history.destroy
     respond_to do |format|
-      format.html { redirect_to pet_histories_url, notice: 'Pet history was successfully destroyed.' }
+      format.html { redirect_to pet_pet_histories_url, notice: 'Pet history was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
